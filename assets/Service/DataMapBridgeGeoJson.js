ca14480755cd301e0bf8a930bec042aeccee3d03
nicola <n.buffo@savnoservizi.it>
2023-10-31 09:17:12 +0100

    Base structure and relationship done

---
+++ assets/Service/DataMapBridgeGeoJson.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { GeoJSON, LayerGroup } from 'react-leaflet';
+import PolylineFlumen from "../component/map/geometries/PolylineFlumen";
+import MarkerNode from "../component/map/geometries/MarkerNode";
+import MarkerPhys from "../component/map/geometries/MarkerPhys";
+import MarkerChemy from "../component/map/geometries/MarkerChemy";
+import hash from 'object-hash';
+import 'leaflet-polylinedecorator';
+
+function DataMapBridgeGeoJson({data, type, viewer}) {
+    const [geojsonData, setGeojsonData] = useState(data);
+    const handleNodeClick = (id, type) => {
+        viewer(id, type)
+    }
+
+    return (
+        <>
+            <LayerGroup key= {hash (geojsonData)}>
+                {type==='line' ? (
+                    data.features.map((polyline, index) => (
+                        <PolylineFlumen
+                            fid={polyline.properties.fid}
+                            positions={polyline.geometry.coordinates}
+                            key={index}
+                            type={polyline.properties.tipo}
+                            name={polyline.properties.nome}
+                            color="blue"
+                            viewer={viewer}
+                            id={index}
+                        />
+                    ))
+                ):''}
+
+                {type==='node' ? (
+                    data.features.map((node,index)=>(
+                        <MarkerNode
+                            key={node.properties.fid}
+                            position={node.geometry.coordinates}
+                            id={node.properties.fid}
+                            viewer={handleNodeClick}
+                        />
+                    ))
+                ): ''}
+
+            </LayerGroup>
+
+        </>
+
+
+        /*
+        *
+        * {type==='phyPoint' ?
+                data.features.map((node,index)=>(
+                    <MarkerPhys
+                        key={index}
+                        position={node.geometry.coordinates}
+                    />
+            )) : ''}
+
+            {type==='chePoint' ? data.features.map((node,index)=>(
+                <MarkerChemy
+                    key={node.properties.fid}
+                    position={node.geometry.coordinates}
+                />
+            )) : ''}
+        *
+        * */
+
+
+    );
+}
+
+
+
+export default DataMapBridgeGeoJson
--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,39 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css'
+import leaflet from 'leaflet';
+import React, { useState } from "react"
+import ReactDOM from "react-dom/client"
+import MapComponents from "./component/MapComponents";
 import './styles/app.css';
 
 
 
-alert('ciao bello')
+
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById('main')
+);
+
+
+const ComponenteConEvento = () => {
+    // Definiamo uno stato iniziale per il nostro componente
+    const [contatore, setContatore] = useState(0);
+
+    // Funzione freccia per gestire l'evento di clic
+    const gestisciClick = () => {
+        setContatore(contatore + 1);
+    };
+
+    return (
+        <div>
+            {/* Aggiungiamo un pulsante che chiama la funzione di gestione dell'evento <button onClick={gestisciClick}>Clicca per Incrementare</button>*/}
+        </div>
+    );
+};
+
+
+
+root.render(<><ComponenteConEvento />
+        <MapComponents />
+</>)
